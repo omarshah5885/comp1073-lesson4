7b6ee559e72e187e2bf28fe8af5ddfb1f00ae787
omarshah5885 <umarshah0@gmail.com>
2016-02-08 15:40:25 -0500

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/*iify or immediately invoked function expression or an anonymous function= a function within a function*/
+( function(){
+/*inside curly braces is the code block
+when called upon, this gets ran
+*/
+	console.log("App Started...");
+
+} )();
