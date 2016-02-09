f04a14b32d15d7f86a9f8c4173a9a3e3c6b18d03
omarshah5885 <umarshah0@gmail.com>
2016-02-09 12:10:18 -0500

    manipulated DOM

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,24 @@
 "use strict";
 
-/*iify or immediately invoked function expression or an anonymous function= a function within a function*/
-( function(){
-/*inside curly braces is the code block
-when called upon, this gets ran
-*/
+//iify or immediately invoked function expression or an anonymous function= a function within a function
+(function () {
+	/*inside curly braces is the code block
+	when called upon, this gets ran
+	*/
 	console.log("App Started...");
+	
+	//named function way
+	function replaceFirstParagraph () {
 
-} )();
+		console.log("named function called");
+		var firstParagraph;
+
+		firstParagraph = document.getElementById("firstParagraph");
+		firstParagraph.innerHTML = "My new paragraph on JS";
+
+
+	}
+	
+	replaceFirstParagraph();
+
+})();
